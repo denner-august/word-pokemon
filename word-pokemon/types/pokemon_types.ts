6a1ac7e2ca91e6ac487102b5ebedce0d93161db8
denner augusto <denner.augusto90@gmail.com>
2023-02-10 03:34:27 -0300

    chore(types): dicionando tipagem dos dados vindo da api para auxiar no desenvolvimento e manutenção

--- word-pokemon/types/pokemon_types.ts
+++ word-pokemon/types/pokemon_types.ts
@@ -1,3 +1,19 @@
-interface pokemonsProps{
-    name: string,
+export interface pokemonStatusProps{
+    stats:{ stat:{name: string},base_stat:number}[]
 }
+
+export interface ContextProps {
+    procura_pokemon:string,
+    setProcura_pokemon: ((value: string) => void)
+    
+    pokemons: {
+        name: string,
+        id: number, stats: [],
+        types: [{ url: string, type: { name: string } }],
+        order:number,
+        sprites: { other: { dream_world: { front_default: string } }, }
+    }[]
+    
+    setPokemons:any
+}
+
