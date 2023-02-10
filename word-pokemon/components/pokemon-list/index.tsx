830b5248d030fcd40a8fe1f7c9549bf1ef0d7b87
denner augusto <denner.augusto90@gmail.com>
2023-02-10 03:39:09 -0300

    component responsavel por renderizar o FetchPokemon

--- word-pokemon/components/pokemon-list/index.tsx
+++ word-pokemon/components/pokemon-list/index.tsx
@@ -1,6 +1,6 @@
 import styles from './card.module.scss'
-import { FetchPokemon } from '@/fetch/swr'
+import { FetchPokemon } from '@/fetch/fetch_pokemon'
 export function CarPokemon() {
 
-    return <ul className={styles.Container}> <FetchPokemon /></ul>
+    return <ul className={styles.Container}> <FetchPokemon /> </ul>
 }
