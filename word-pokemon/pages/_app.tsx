68e6824e17760b55790ceb6b20c3b7546eff5b74
denner augusto <denner.augusto90@gmail.com>
2023-02-10 03:42:40 -0300

    conecta o context criado ao resto da aplicação

--- word-pokemon/pages/_app.tsx
+++ word-pokemon/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ContextPokemonProvider } from '@/context/context_Pokemon'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ContextPokemonProvider>
+      <Component {...pageProps} />
+    </ContextPokemonProvider>
+  )
 }
